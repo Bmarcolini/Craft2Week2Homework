91548135426b64f3a90253aff38c568dda3e7172
BARBARA MARCOLINI <BARBARA.MARCOLINI@JOURNALISM.CUNY.EDU>
2016-02-14 23:00:14 -0500

    (subindo homework)

---
+++ java.js
@@ -0,0 +1,13 @@
+var x = 10;
+var y = 5;
+
+var a = x + y;
+var b = x - y;
+var c = x * y;
+var d = x / y;
+
+
+document.getElementById("add").innerHTML= "x + y =" + a;
+document.getElementById("subtract").innerHTML= "x - y =" + b;
+document.getElementById("multiply").innerHTML= "x * y =" + c; 
+document.getElementById("divide").innerHTML= "x / y =" + d;
